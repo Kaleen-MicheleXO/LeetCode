cbb175d65a382d616be691b9f873e97dbc7f5e51
Kaleen Moon <kaleen.moon1@gmail.com>
2023-01-05 19:20:52 -0500

    Sum of Unique Elements

--- LeetCode01-05-23.js
+++ LeetCode01-05-23.js
@@ -1,34 +1,40 @@
-1748. Sum of Unique Elements
-Easy
-1.1K
-24
-Companies
-You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+// 1748. Sum of Unique Elements
+// Easy
+// 1.1K
+// 24
+// Companies
+// You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
 
-Return the sum of all the unique elements of nums.
+// Return the sum of all the unique elements of nums.
 
- 
+// Example 1:
 
-Example 1:
+// Input: nums = [1,2,3,2]
+// Output: 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+// Example 2:
 
-Input: nums = [1,2,3,2]
-Output: 4
-Explanation: The unique elements are [1,3], and the sum is 4.
-Example 2:
+// Input: nums = [1,1,1,1,1]
+// Output: 0
+// Explanation: There are no unique elements, and the sum is 0.
+// Example 3:
 
-Input: nums = [1,1,1,1,1]
-Output: 0
-Explanation: There are no unique elements, and the sum is 0.
-Example 3:
+// Input: nums = [1,2,3,4,5]
+// Output: 15
+// Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
 
-Input: nums = [1,2,3,4,5]
-Output: 15
-Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
- 
+// Constraints:
 
-Constraints:
-
-1 <= nums.length <= 100
-1 <= nums[i] <= 100
-Accepted
-108.5K
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+// Accepted
+// 108.5K
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function (nums) {
+  return nums
+    .map((x, i, a) => (a.indexOf(x) === a.lastIndexOf(x) ? x : 0))
+    .reduce((sum, curr) => sum + curr, 0);
+};
